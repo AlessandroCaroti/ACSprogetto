90dad0de1fa2a8fdfb8ba30ef228314ae8bcb4d1
strafoxultrapowa <aspem2015@gmail.com>
2018-05-23 13:32:44 +0200

    edited ResponseCode class

--- progetto/Shared/src/utility/Account.java
+++ progetto/Shared/src/utility/Account.java
@@ -6,34 +6,46 @@ import Interfaces.ServerInterface;
 
 public class Account {
     private String username;
-    private String password;    //hash password
+    private int password;//hash password
     private ClientInterface stub;
     private String publicKey;
+    private static hashFunctions hashClass=new hashFunctions();
 
+
+    /* TODO */
     /**
      * Crea un nuovo account
      * @param userName l'username :D
      * @param plainPassword (password in chiaro che verrà criptata)
      * @param stub lo del client
      * @param  publicKey la chiave pubblica del client
-     * @throws NullPointerException() se username o password corrispondono a null gli altri possono essere passati come null
+     * @throws NullPointerException() se username o password corrispondono a null;gli altri possono essere passati come null
      */
     public Account(String userName, String plainPassword, ClientInterface stub, String publicKey)
-    throws  NullPointerException
-    {
-        if(userName==null||plainPassword==null)
-        {
+    throws  NullPointerException {
+        if (userName == null || plainPassword == null) {
             throw new NullPointerException("username o plainpassword == null");
+        } else {
+            this.username = userName;
+            this.password = hashClass.stringHash(plainPassword);
+            this.stub = stub;
+            this.publicKey = publicKey;
         }
-        this.username=userName;
-        this.password="ciao";
-        this.stub=stub;
-        this.publicKey=publicKey;
     }
 
-    public boolean cmpPassword(String password)
+
+    /**
+     * Compara la password in chiaro passata (successivamente hashata con l'hash di quella salvata
+     * @param plainPassword password in chiaro, se null viene settata come stringa vuota
+     * @return true se corrispondono, false altrimenti
+     */
+    public boolean cmpPassword(String plainPassword)
     {
-        
-        return false;
+        if(plainPassword==null)
+        {
+            plainPassword="";
+        }
+        return this.password==hashClass.stringHash(plainPassword);
+
     }
 }
--- progetto/Shared/src/utility/hashFunctions.java
+++ progetto/Shared/src/utility/hashFunctions.java
@@ -1,5 +1,21 @@
 package utility;
 
 public class hashFunctions {
+    protected hashFunctions(){
+    }
+
+    /**Cripta la string passata
+     * @param plainText il testo in chiaro
+     * @return l'hash della stringa passata
+     * @throws NullPointerException se plainText è null
+     */
+    public int stringHash(String plainText) throws NullPointerException
+    {
+        if(plainText==null)
+        {
+            throw new NullPointerException("plainText == null");
+        }
+        return plainText.hashCode();
+    }
 
 }
