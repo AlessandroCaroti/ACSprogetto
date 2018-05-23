de3ff8872b3dab4ceb2248390c01239318e14213
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-05-23 12:30:41 +0200

    creata la classe Topic

--- progetto/Shared/src/utility/Topic.java
+++ progetto/Shared/src/utility/Topic.java
@@ -1,4 +1,27 @@
 package utility;
 
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Topic {
+    private String topicName;
+    private int id_topic;
+    static private AtomicInteger serial_id  = new AtomicInteger();
+    static boolean reachedMaxNumTopic = false;
+
+    public Topic(String topicName) throws Exception {
+        if(reachedMaxNumTopic)
+            throw new Exception();
+        this.topicName = topicName;
+        id_topic = serial_id.incrementAndGet();
+        if(id_topic == 0) {
+            reachedMaxNumTopic = true;
+            throw new Exception();
+        }
+
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
 }
