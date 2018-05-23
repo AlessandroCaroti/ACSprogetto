1ecf27569fa7d7851c29874e600c2aaccc3ce3b9
Gabriele Armanino <gabry_arma@hotmail.it>
2018-05-23 13:30:50 +0200

    edited Client,Server and Message Classes

--- progetto/ClientProject/src/client/Client.java
+++ progetto/ClientProject/src/client/Client.java
@@ -4,28 +4,47 @@ import Interfaces.ClientInterface;
 import Interfaces.ServerInterface;
 import utility.Message;
 import utility.ResponseCode;
+import utility.Topic;
 
 import java.rmi.RemoteException;
 
 public class Client implements ClientInterface {
-    //STRUTTURE DATI
+
+    //Variabili di istanza
+
     private String username;
     private String light_password;
     private ClientInterface stub;
     private ServerInterface skeleton;//broker stub
-    private static long cookie;
+    private long cookie;
     private String bp_key; //broker public key
     private String my_private_key;
+    private static long last_cookie;
+
+    /*************************************************************************************************************/
+    //Costruttore
+
+    public Client(String username, String light_password, ClientInterface stub, ServerInterface skeleton,
+                  String bp_key, String my_private_key)
+    {
+        if(username==null||light_password==null||stub==null||skeleton==null||bp_key==null||my_private_key==null)
+            throw new NullPointerException();
+
+        this.username=username;
+        this.light_password=light_password;
+        this.stub=stub;
+        this.skeleton=skeleton;
+        this.cookie=last_cookie++;
+        this.bp_key=bp_key;
+        this.my_private_key=my_private_key;
+    }
 
     /*************************************************************************************************************/
     //API
 
     /*TODO
-    aggiungere i metodi elencari nel file che spcifica le API del client
+    aggiungere i metodi elencari nel file che specifica le API del client
      */
-
-
-
 
 
     /*************************************************************************************************************/
@@ -33,7 +52,14 @@ public class Client implements ClientInterface {
 
     @Override
     public ResponseCode notify(Message m) throws RemoteException {
-        return null;
+        if(m==null) {
+            ResponseCode rc=new ResponseCode(ResponseCode.Codici.R400, ResponseCode.TipoClasse.CLIENT,
+                    "NOT OK Il server ha ricevuto un messaggio vuoto");
+            return rc;
+        }
+        ResponseCode rc=new ResponseCode(ResponseCode.Codici.R200, ResponseCode.TipoClasse.CLIENT,
+                "OK il server ha ricevuto il messaggio");
+        return rc;
     }
 
     @Override
