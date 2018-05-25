7a3403e7457bcfb6de0ae3e4a430c894b02704f7
strafoxultrapowa <aspem2015@gmail.com>
2018-05-25 10:55:48 +0200

    edited ResponseCode class

--- progetto/Shared/src/utility/Topic.java
+++ progetto/Shared/src/utility/Topic.java
@@ -1,27 +1,50 @@
 package utility;
 
-
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Topic {
     private String topicName;
     private int id_topic;
-    static private AtomicInteger serial_id  = new AtomicInteger();
-    static boolean reachedMaxNumTopic = false;
+    static private int serial_id;
+    static private boolean reachedMaxNumTopic = false;
 
     public Topic(String topicName) throws Exception {
-        if(reachedMaxNumTopic)
-            throw new Exception();
-        this.topicName = topicName;
-        id_topic = serial_id.incrementAndGet();
-        if(id_topic == 0) {
-            reachedMaxNumTopic = true;
-            throw new Exception();
+
+        this.id_topic=getAndIncrementTopicId();
+        if(topicName==null)
+        {
+            this.topicName="UNKNOWN_TOPIC";
         }
+        else{
+            this.topicName=topicName;
+        }
+
 
     }
 
     public String getTopicName() {
         return topicName;
+    }
+    public int getId_topic(){return this.id_topic;}
+
+    /**
+     * Ritorna il nuovo serial id,se  reached max num topic-> false   incrementa di uno serial_id
+     * @return topicid
+     * @throws Exception se il numero massimo di topic è stato raggiunto
+     */
+    private synchronized int getAndIncrementTopicId() throws Exception
+    {
+        int id;
+        if(reachedMaxNumTopic){
+            throw new Exception("ERROR:MAX TOPIC NUMBER REACHED");
+        }
+        else
+        {
+            id=++serial_id;
+            if (id==0)//overflow
+            {
+                reachedMaxNumTopic=true;
+                throw new Exception("ERROR:MAX TOPIC NUMBER REACHED");
+            }
+            return id;
+        }
     }
 }
