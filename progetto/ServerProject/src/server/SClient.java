94e40a96405eb2a851f2e94d582758c7a9100a6a
strafoxultrapowa <aspem2015@gmail.com>
2018-05-29 10:33:22 +0200

    Sclient class modified

--- progetto/ServerProject/src/server/SClient.java
+++ progetto/ServerProject/src/server/SClient.java
@@ -1,6 +1,55 @@
 package server;
 
 import client.Client;
+import interfaces.ClientInterface;
+import interfaces.ServerInterface;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.Properties;
 
 public class SClient extends Client {
+    private ServerInterface[] brokersStubs;
+    private static final int DEFAULTCONNECTIONNUMBER=10;
+
+    public SClient(String username, String plainPassword, ClientInterface skeleton, String bp_key, String my_private_key)
+            throws RemoteException
+    {
+        super(username,plainPassword,skeleton,bp_key,my_private_key);
+
+        try {
+            Properties sClientSettings = new Properties();//setting del server
+            FileInputStream in = new FileInputStream("config.serverSettings");
+            sClientSettings.load(in);
+            in.close();
+            this.brokersStubs = new ServerInterface[Integer.parseInt(sClientSettings.getProperty("maxbrokerconnection"))];
+        }catch(IOException exc){
+            System.err.println("ERROR:unable to open or read config.serverSettings");
+            this.brokersStubs=new ServerInterface[DEFAULTCONNECTIONNUMBER];
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
