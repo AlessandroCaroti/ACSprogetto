3a13b893ea80694ce83e7ec679d22422011f5732
strafoxultrapowa <aspem2015@gmail.com>
2018-05-30 13:57:16 +0200

    Created testclass for accountMonitor

---
+++ progetto/ServerProject/src/test/accountMonitorTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import server.AccountCollectionInterface;
+
+public class accountMonitorTest {
+
+    public static void main(String[] args)
+    {
+        AccountCollectionInterface accountMonitor;
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
