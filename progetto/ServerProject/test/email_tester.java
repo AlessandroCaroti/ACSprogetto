947401dcb0010fd6c996e217ce4f6b1049d4eb13
strafoxultrapowa <aspem2015@gmail.com>
2018-06-18 13:51:52 +0200

    implementing tester for emailHandlerClass

--- progetto/ServerProject/test/email_tester.java
+++ progetto/ServerProject/test/email_tester.java
@@ -1,12 +1,21 @@
+import email.EmailHandler;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class email_tester {
 
     public static void main(String[] args) {
     try{
-
-
+        FileInputStream in=new FileInputStream("localsettings");
+        Properties properties=new Properties();
+        properties.load(in);
+        EmailHandler emailHandler=new EmailHandler(properties,100);
+        emailHandler.addMessage(emailHandler.createEmailMessage("ACSgroup.unige@gmail.com","ALUA","ciaozi"));
         }catch(Exception exc){
             exc.printStackTrace();
         }
     }
 
 }
+
