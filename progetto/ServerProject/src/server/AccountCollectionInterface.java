7a738f0c6e14d115929fda357f703bb38c1f04e7
strafoxultrapowa <aspem2015@gmail.com>
2018-06-19 13:15:13 +0200

    minor change

--- progetto/ServerProject/src/server/AccountCollectionInterface.java
+++ progetto/ServerProject/src/server/AccountCollectionInterface.java
@@ -1,4 +1,4 @@
-/**
+/*
     This file is part of ACSprogetto.
 
     ACSprogetto is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with ACSprogetto.  If not, see <http://www.gnu.org/licenses/>.
 
-**/
+*/
 package server;
 
 import customException.AccountMonitorRuntimeException;
