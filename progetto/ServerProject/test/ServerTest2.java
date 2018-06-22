97a8159d03cf1f099374150662cf9314dcf96c50
strafoxultrapowa <aspem2015@gmail.com>
2018-06-22 15:33:44 +0200

    added ServerTest2 class

--- progetto/ServerProject/test/ServerTest2.java
+++ progetto/ServerProject/test/ServerTest2.java
@@ -1,2 +1,22 @@
+import server.Server;
+import utility.ServerInfoProvider;
+
+import java.net.InetAddress;
+
+
 public class ServerTest2 {
+    static Server broker;
+    public static void main(String[] args) {
+        try {
+            broker = new Server();
+            broker.start();
+            SecurityManager security = System.getSecurityManager();
+
+
+        }catch (Exception e){
+            e.getClass().getSimpleName();
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
 }
