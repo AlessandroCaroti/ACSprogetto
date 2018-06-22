c3581d9fca583ade80e7f5a0adeab71dc0c366ed
strafoxultrapowa <aspem2015@gmail.com>
2018-06-22 23:55:10 +0200

    testing

--- progetto/ClientProject/test/clientTest_2.java
+++ progetto/ClientProject/test/clientTest_2.java
@@ -2,18 +2,22 @@ import client.Client;
 import utility.ServerInfoRecover;
 
 public class clientTest_2 {
-    static Client c;
+    static Client client;
     public static void main(String[] args) {
         try {
-            c = new Client("user_1", "password","pki_pubblica", "pki_privata");
+            client = new Client("user_1", "password","pki_pubblica", "pki_privata");
             try {
                 ServerInfoRecover infoServer = new ServerInfoRecover();
                 String[] a = infoServer.getServerInfo();
-                c.setServerInfo(a[0], Integer.valueOf(a[1]), a[2]);
+                client.setServerInfo(a[0], Integer.valueOf(a[1]), a[2]);
             }catch (Exception e){
                 System.out.println("No server visible in the local network");
                 e.printStackTrace();
+                return;
             }
+            if(client.register()){//TODO lo stub non viene settato bisogna fare in modo che la infoserver.getServerInfo faccia il set
+                System.out.println("REGISTRATO");
+            }else{System.out.println("NON REGISTRATO");return;}
 
         }catch (Exception e){
 
