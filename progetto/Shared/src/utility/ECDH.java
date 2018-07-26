4fd2a5a5d41813f54018409b2aed26d542d5c14b
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-07-26 10:40:36 +0200

    aggiunta classe per Elliptic-curve Diffie–Hellman

---
+++ progetto/Shared/src/utility/ECDH.java
@@ -0,0 +1,29 @@
+package utility;
+
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+
+import javax.crypto.KeyAgreement;
+import java.security.*;
+
+final public class ECDH {
+    
+    public static KeyPair generateKeyPair(String curveName) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        Security.addProvider(new BouncyCastleProvider());
+        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec(curveName);
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDH", "BC");
+        keyGen.initialize(ecSpec, new SecureRandom());
+
+        return keyGen.generateKeyPair();
+    }
+
+    public static byte[] sheredSecretKey(PrivateKey myPrivateKey, PublicKey b) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException {
+        KeyAgreement keyAgree = KeyAgreement.getInstance("ECDH", "BC");
+        keyAgree.init(myPrivateKey);
+        keyAgree.doPhase(b, true);
+
+        return keyAgree.generateSecret();
+    }
+
+}
