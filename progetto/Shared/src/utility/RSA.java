4f6748beb7d66b43d82371c15d664c1531db6d59
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-07-26 12:32:12 +0200

    minor changes

--- progetto/Shared/src/utility/RSA.java
+++ progetto/Shared/src/utility/RSA.java
@@ -6,20 +6,20 @@ import java.security.*;
 final public class RSA {
     static private final int keySize = 2048;
 
-    public static KeyPair generateRSA_keyPair() throws NoSuchAlgorithmException {
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(keySize);
         return keyPairGenerator.genKeyPair();
     }
 
-    public static byte[] encryptRSA(PrivateKey privateKey, byte [] plainBytes) throws Exception {
+    public static byte[] encrypt(PrivateKey privateKey, byte [] plainBytes) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, privateKey);
 
         return cipher.doFinal(plainBytes);
     }
 
-    public static byte[] decryptRSA(PublicKey publicKey, byte [] encryptedBytes) throws Exception {
+    public static byte[] decrypt(PublicKey publicKey, byte [] encryptedBytes) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, publicKey);
 
