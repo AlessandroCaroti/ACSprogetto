492e0ec1f2588bccffc7ec9eb82f9a338d942ba0
strafoxultrapowa <aspem2015@gmail.com>
2018-07-28 13:45:22 +0200

    minor changes

--- progetto/ServerProject/src/server/Server.java
+++ progetto/ServerProject/src/server/Server.java
@@ -100,7 +100,7 @@ public class Server implements ServerInterface,Callable<Integer> {
      */
 
 
-    public Server() throws InvalidKeyException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, UnsupportedEncodingException, AlreadyBoundException, RemoteException, UnknownHostException {
+    public Server() throws InvalidKeyException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, UnsupportedEncodingException {
 
 
         try{
