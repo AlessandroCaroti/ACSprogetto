5763f2f51080c6eb09d2262a0c6e93bdcf70d9a3
strafoxultrapowa <aspem2015@gmail.com>
2018-07-28 13:43:32 +0200

    minor changes

--- progetto/ServerProject/src/server/Server.java
+++ progetto/ServerProject/src/server/Server.java
@@ -102,7 +102,7 @@ public class Server implements ServerInterface,Callable<Integer> {
 
     public Server() throws InvalidKeyException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, UnsupportedEncodingException, AlreadyBoundException, RemoteException, UnknownHostException {
 
-        
+
         try{
             serverName   = "Server_" + this.getMyIp();
         }catch(IOException exc){//se non riesce a reperire  l'ip
