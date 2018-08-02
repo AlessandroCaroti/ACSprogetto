201be55174e74f908a3df615e17b42e87163428a
strafoxultrapowa <aspem2015@gmail.com>
2018-08-02 22:07:38 +0200

    working on ClientEngine

---
+++ progetto/ClientProject/src/Events/ForumWindow.java
@@ -0,0 +1,13 @@
+package Events;
+
+
+import static guiClient.WindowType.FORUM;
+
+public class ForumWindow extends Window {
+
+    //todo bisogna aggiungere i buffer dove arrivano le notifiche eccetera
+    public ForumWindow(){
+        this.setWindowType(FORUM);
+    }
+
+}
