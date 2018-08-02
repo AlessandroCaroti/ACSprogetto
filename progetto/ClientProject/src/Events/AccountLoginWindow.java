1a86aace87030a0b51ac2668f470fc55e713e081
strafoxultrapowa <aspem2015@gmail.com>
2018-08-02 19:30:40 +0200

    working on clientEngine

--- progetto/ClientProject/src/Events/AccountLoginWindow.java
+++ progetto/ClientProject/src/Events/AccountLoginWindow.java
@@ -2,21 +2,24 @@ package Events;
 import static guiClient.WindowType.LOGIN;
 
 public class AccountLoginWindow extends AnonymousLoginWindow {
-    private String usernameOrEmail;
+    private String username;
     private String password;
+    private String email;
 
     public AccountLoginWindow(){
         this.setWindowType(LOGIN);
     }
 
+    public String getEmail() { return email; }
 
+    public void setEmail(String email) { this.email = email; }
 
-    public String getUsernameOrEmail() {
-        return usernameOrEmail;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUsernameOrEmail(String usernameOrEmail) {
-        this.usernameOrEmail = usernameOrEmail;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
