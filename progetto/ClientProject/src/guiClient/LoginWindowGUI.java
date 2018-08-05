cfb8945512e10cd886ae6b43249514451a98e6b8
strafoxultrapowa <aspem2015@gmail.com>
2018-08-05 13:38:50 +0200

    added new class

---
+++ progetto/ClientProject/src/guiClient/LoginWindowGUI.java
@@ -0,0 +1,58 @@
+package guiClient;
+
+import javax.swing.*;
+
+
+public class LoginWindowGUI {
+
+    private JPanel jPanel1;
+    private JPanel login;
+    private JPanel panel;
+
+    private JLabel lblNewLabel_3;
+    private JLabel lblNewLabel_4;
+    private JLabel lblNewLabel_5;
+    private JLabel lblNewLabel_6;
+    private JLabel lblNewLabel_7;
+    private JLabel btn_login;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JLabel lblPassword;
+    private JLabel lblEmail_1;
+    private JLabel lblServerPortAddress;
+    private JLabel lblServerIpAddress;
+    private JLabel lblAnonymous;
+
+
+
+    private JSeparator jSeparator1;
+    private JSeparator separator_3;
+    private JSeparator separator_6;
+    private JSeparator separator_7;
+
+
+    private JTextField txtEmail;
+    private JTextField txtServerPortAddress;
+    private JTextField tctServerIpAddress;
+
+
+
+    public LoginWindowGUI(){
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
