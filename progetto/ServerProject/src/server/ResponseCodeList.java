03941c126105d6cf3975dcc730bcadb38800e10b
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-09-09 18:27:22 +0200

    Minor changes

--- progetto/ServerProject/src/server/ResponseCodeList.java
+++ progetto/ServerProject/src/server/ResponseCodeList.java
@@ -12,5 +12,6 @@ class ResponseCodeList {
     final static ResponseCode WrongCodeValidation = new ResponseCode(ResponseCode.Codici.R650, ResponseCode.TipoClasse.SERVER, "The entered code is incorrect! Attempts terminated, repeat the procedure.");
     final static ResponseCode InvalidUsername = new ResponseCode(ResponseCode.Codici.R660, ResponseCode.TipoClasse.SERVER, "Username already in use! Repeat the procedure.");
     final static ResponseCode CookieNotFound = new ResponseCode(ResponseCode.Codici.R666, ResponseCode.TipoClasse.SERVER, "Cookies not recognize.");
+    final static ResponseCode TopicNotFound = new ResponseCode(ResponseCode.Codici.R666, ResponseCode.TipoClasse.SERVER, "Topic do not exist in the server.");
     final static ResponseCode BrokenConnection = null;
 }
