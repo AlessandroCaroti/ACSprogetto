8bd5ca4751e4adb135182400e1a97091ae6bb556
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-09-12 19:26:40 +0200

    Impostato il metodo di pulizia di una lista. NON ANCORA TESTATO!

--- progetto/ServerProject/src/server/utility/ListCleaner.java
+++ progetto/ServerProject/src/server/utility/ListCleaner.java
@@ -1,5 +1,11 @@
 package server.utility;
 
+import account.AccountCollectionInterface;
+import client.AnonymousClient;
+import interfaces.ClientInterface;
+
+import java.rmi.RemoteException;
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -8,10 +14,15 @@ import java.util.concurrent.ConcurrentSkipListSet;
 public class ListCleaner {
     final private ConcurrentSkipListMap<String, ConcurrentSkipListSet<Integer>> list;
     final private ConcurrentSkipListSet<Integer> accountOffline;
+    final private AccountCollectionInterface accountList;
     final private Timer timer;
+    final private AnonymousClient fakeClient;
 
-    public ListCleaner(ConcurrentSkipListMap<String, ConcurrentSkipListSet<Integer>> list) {
-        this.list = list;
+    public ListCleaner(ConcurrentSkipListMap<String, ConcurrentSkipListSet<Integer>> list, AccountCollectionInterface accountList) throws RemoteException {
+        Objects.requireNonNull(this.list = list);
+        Objects.requireNonNull(this.accountList = accountList);
+
+        fakeClient = new AnonymousClient();
         accountOffline = new ConcurrentSkipListSet<>();
         timer = new Timer(true);
         timer.schedule(new TimerTask() {
@@ -27,9 +38,19 @@ public class ListCleaner {
     }
 
     public void cleanList() {
+        Thread.currentThread().setPriority(4);
+        ClientInterface fakeStub = fakeClient.getSkeleton();
         for (Integer accountId : accountOffline) {
-
-
+            ClientInterface prev = accountList.setStub(fakeStub, accountId);
+            if (prev != null)   //account in utilizzo
+                continue;
+            //todo rimuovere l'account corrente dalle varie liste
+            String[] topicSubscribed = accountList.getTopicSubscribed(accountId);
+            for (String topic: topicSubscribed){
+                list.get(topic).remove(accountId);
+            }
+            accountList.setStub(null, accountId, true);
+//            Thread.yield();
         }
         accountOffline.clear();
     }
