919b91384d297dc3519120c064111b03dd30a986
Alessandro <AlessandroCaroti@users.noreply.github.com>
2018-09-12 18:57:02 +0200

    minor changes

--- progetto/ServerProject/src/server/ListCleaner.java
+++ progetto/ServerProject/src/server/ListCleaner.java
@@ -1,29 +1,36 @@
 package server;
 
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 public class ListCleaner {
     final private ConcurrentSkipListMap<String, ConcurrentSkipListSet<Integer>> list;
-    final private ConcurrentSkipListSet<String> topicToClean;
+    final private ConcurrentSkipListSet<Integer> accountOffline;
     final private Timer timer;
 
     public ListCleaner(ConcurrentSkipListMap<String, ConcurrentSkipListSet<Integer>> list) {
         this.list = list;
-        topicToClean = new ConcurrentSkipListSet<>();
+        accountOffline = new ConcurrentSkipListSet<>();
         timer = new Timer(true);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                cleanList();
+            }
+        }, 15*60*1000L,5*60*1000);
     }
 
-    void topicListToClean(String topicName) {
-        topicToClean.add(topicName);
+    public void addClientOffline(int accountId) {
+        accountOffline.add(accountId);
     }
 
-    void cleanList() {
-        for (String topic : topicToClean) {
-            ConcurrentSkipListSet<Integer> subscribers = list.get(topic);
+    public void cleanList() {
+        for (Integer accountId : accountOffline) {
+
 
         }
-        topicToClean.clear();
+        accountOffline.clear();
     }
 }
