5da1e5d11f89626bb1dc4d524244860dc0756e92
caroti.ale@gmail.com <romeoeva22>
2018-10-01 19:35:11 +0200

    miglioramenti

--- progetto/ServerProject/src/server_gui/Connection.java
+++ progetto/ServerProject/src/server_gui/Connection.java
@@ -1,6 +1,6 @@
 package server_gui;
 
-import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,25 +10,22 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.border.TitledBorder;
 
-import com.sun.xml.internal.ws.api.Component;
-
 import utility.ServerInfo;
 import utility.infoProvider.ServerInfoRecover;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
-import java.awt.Color;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingWorker;
 import javax.swing.JButton;
-import java.awt.Dimension;
 import javax.swing.JScrollPane;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.HashMap;
-import java.awt.GridLayout;
-import java.awt.TextField;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class Connection extends JFrame {
 
@@ -36,6 +33,7 @@ public class Connection extends JFrame {
 	private JPanel ServerList_panel;
 	private JTextField textField_port;
 	private JTextField textField_address;
+	private JTextField textField_name;
 
 	/**
 	 * Launch the application.
@@ -44,7 +42,7 @@ public class Connection extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Connection frame = new Connection();
+					Connection frame = new Connection("");
 					frame.setTitle("Connection Manager");
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -57,10 +55,14 @@ public class Connection extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Connection() {
-		setMinimumSize(new Dimension(340, 300));
+	final private String myName;
+	private JButton btnUpdate;
+	
+	public Connection(String myName) {
+		Objects.requireNonNull(this.myName = myName);
+		setMinimumSize(new Dimension(390, 340));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 341, 492);
+		setBounds(100, 100, 390, 446);
 		contentPane = new JPanel();
 		contentPane.setMinimumSize(new Dimension(340, 10));
 		contentPane.setBorder(null);
@@ -77,53 +79,32 @@ public class Connection extends JFrame {
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 329, Short.MAX_VALUE)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 329, Short.MAX_VALUE))
-					.addGap(4))
-				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(12)
-					.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 309, Short.MAX_VALUE)
+					.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 358, Short.MAX_VALUE)
 					.addContainerGap())
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 378, Short.MAX_VALUE)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 378, Short.MAX_VALUE))
+					.addGap(4))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
 					.addGap(22)
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 271, Short.MAX_VALUE)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 334, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		
-		JButton btnNewButton = new JButton("UPDATE");
-		btnNewButton.addMouseListener(new MouseAdapter() {
+		btnUpdate = new JButton("UPDATE");
+		btnUpdate.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				ServerInfoRecover infoServer;
-		        HashMap<String, String[]> servers;
-
-		        //RICERCA DEI SERVER DISPONIBILI NELLA RETE LOCALE
-		        try {
-		            infoServer = new ServerInfoRecover(false);
-		        } catch (IOException e1) {
-		            return;
-		        }
-		        servers = infoServer.findAllServerOnLan();
-
-		        //RIMOZIONE DEI VECCHI SERVER
-		        ServerList_panel.removeAll();
-		        ServerList_panel.revalidate();
-		        ServerList_panel.repaint();
-		        
-		        //VISUALIZAZIONE DEI SERVER TROVATI
-		        String[] serverFound = servers.keySet().toArray(new String[0]);
-		        for(String server: serverFound) {
-		        	String[] curServer = servers.get(server);
-		        	ServerList_panel.add(new ServerInfoPanel(server, new ServerInfo(curServer[2], Integer.parseInt(curServer[1]))));
-		        }		        
+				updateList();
 			}
 		});
 		
@@ -133,14 +114,14 @@ public class Connection extends JFrame {
 			gl_panel_1.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addContainerGap(217, Short.MAX_VALUE)
-					.addComponent(btnNewButton)
+					.addComponent(btnUpdate)
 					.addContainerGap())
 				.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 317, Short.MAX_VALUE)
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
-					.addComponent(btnNewButton)
+					.addComponent(btnUpdate)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE))
 		);
@@ -160,6 +141,11 @@ public class Connection extends JFrame {
 		
 		textField_port = new JTextField();
 		textField_port.setColumns(10);
+		
+		JLabel lblServerName = new JLabel("Server Name:");
+		
+		textField_name = new JTextField();
+		textField_name.setColumns(10);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
@@ -167,19 +153,33 @@ public class Connection extends JFrame {
 					.addGap(6)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(lblServerIp, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblServerIp, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblServerPort, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE))
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textField_address, GroupLayout.DEFAULT_SIZE, 182, Short.MAX_VALUE))
-						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-							.addComponent(lblServerPort, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
-							.addGap(25)
-							.addComponent(textField_port, GroupLayout.DEFAULT_SIZE, 169, Short.MAX_VALUE)))
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(textField_port, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.RELATED))
+								.addComponent(textField_address, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE)))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblServerName, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(textField_name, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE)))
 					.addGap(0))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(21)
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(8)
+							.addComponent(lblServerName))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(6)
+							.addComponent(textField_name, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblServerIp)
 						.addComponent(textField_address, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
@@ -187,10 +187,15 @@ public class Connection extends JFrame {
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblServerPort)
 						.addComponent(textField_port, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(31))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		panel.setLayout(gl_panel);
 		contentPane.setLayout(gl_contentPane);
+	}
+	
+	private void updateList() {
+		ServerSearchWorker worker = new ServerSearchWorker();
+		worker.execute();
 	}
 	
 	private class ServerInfoPanel extends JPanel {
@@ -210,6 +215,7 @@ public class Connection extends JFrame {
 					if(e.getClickCount() == 2) {
 						textField_address.setText(info.getRegHost());
 						textField_port.setText(String.valueOf(info.getRegPort()));
+						textField_name.setText(name);
 						return;
 					}
 					java.awt.Component[] components = ServerList_panel.getComponents();
@@ -224,15 +230,15 @@ public class Connection extends JFrame {
 					e.getComponent().setBackground(Color.CYAN);
 				}
 			});
-			setPreferredSize(new Dimension(10, 30));
+			setPreferredSize(new Dimension(10, 40));
 			setBorder(new CompoundBorder(new EmptyBorder(1, 1, 1, 1), new LineBorder(new Color(0, 0, 0), 1, true)));
 			setBackground(Color.WHITE);
-			setMinimumSize(new Dimension(10, 30));
-			setMaximumSize(new Dimension(32767, 30));
+			setMinimumSize(new Dimension(10, 40));
+			setMaximumSize(new Dimension(32767, 40));
+			setLayout(new BorderLayout(0, 0));
 
 			this.name = name;
 			this.info = info;
-			setLayout(new GridLayout(0, 2, 0, 0));
 			
 			lblServername = new JLabel(this.name);
 			add(lblServername);
@@ -242,5 +248,50 @@ public class Connection extends JFrame {
 		public ServerInfo getInfo() {
 			return info;
 		}	
+	}
+	
+	private class ServerSearchWorker extends SwingWorker<HashMap<String, String[]>, Void>{
+		
+		@Override
+		protected HashMap<String, String[]> doInBackground() throws Exception {
+			btnUpdate.setEnabled(false);
+			btnUpdate.setText("Serching");				
+			
+			//RIMOZIONE DEI VECCHI SERVER
+	        ServerList_panel.removeAll();
+	        ServerList_panel.revalidate();
+	        ServerList_panel.repaint();		
+
+	        //RICERCA DEI SERVER DISPONIBILI NELLA RETE LOCALE
+	        try {
+	        	ServerInfoRecover infoServer = new ServerInfoRecover(false);
+	        	return infoServer.findAllServerOnLan();
+	        } catch (IOException e1) {
+	            return null;
+	        }
+		}
+		
+		@Override
+		protected void done() {
+			btnUpdate.setText("Update");
+			btnUpdate.setEnabled(true);		
+	        
+	        //VISUALIZAZIONE DEI SERVER TROVATI
+	        try {
+				HashMap<String, String[]> servers = get();
+				if(servers==null)
+					return;
+				String[] serverFound = servers.keySet().toArray(new String[0]);
+		        for(String server: serverFound) {
+		        	if(server.equals(myName))
+		        		continue;
+		        	String[] curServer = servers.get(server);
+		        	ServerList_panel.add(new ServerInfoPanel(server, new ServerInfo(curServer[0], Integer.parseInt(curServer[1]))));
+		        }
+			} catch (InterruptedException | ExecutionException ignored) {
+			}
+		}
+		
+		
 	}
 }
